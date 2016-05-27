be040e889810e8d4a5618f86236d52ec299b3aa4
mi-yu <m.yu8088@gmail.com>
2016-05-27 07:54:28 -0500

    added icon shift to landing page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var paused = false;
+var i=0;
+shiftIcon();
+setInterval(shiftIcon,1500);
+function shiftIcon() {
+	if (!paused){
+		$('#landing-icon').attr('src','./dist/imgs/landing-icons/'+i+'.svg');
+		$('#landing-icon').css({"opacity":0});
+		$('#landing-icon').animate({
+			opacity: 1,
+		}, 500);
+		if (i==29) {i=0;}
+		else i++;
+	}
+}
