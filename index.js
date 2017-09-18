827b7c452c21759c06b2aa96c16254cd54ae14bb
Thomas Kaldahl <tbk283@gmail.com>
2017-09-17 19:42:44 -0500

    dank 404 messages

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use(function(req, res, next) {
 	res.status(400);
-	res.send('<a href="."><img src="dist/imgs/404.png" style="width:90vw"></a>');
+	res.send(
+		'<a href=".."><img style="max-width:100vw;max-height:100vh" src="./dist/imgs/404/' +
+		Math.ceil(9 * Math.random()) +
+		'.jpg" style="width:90vw"></a>'
+	);
 });
 
 app.listen(app.get('port'), function() {
